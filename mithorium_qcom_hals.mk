6a8f95e7f2c599a0f75a140e436746d13a224ef4
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-19 20:15:03 +0800

    Add new qcom display repos to product soong namespaces

--- mithorium_qcom_hals.mk
+++ mithorium_qcom_hals.mk
@@ -4,3 +4,7 @@ MITHORIUM_DEVICE_SPECIFIC_HALS := audio display media
 $(foreach h, $(call to-upper, $(MITHORIUM_DEVICE_SPECIFIC_HALS)), \
     $(eval USE_DEVICE_SPECIFIC_$(h) := true) \
     $(eval DEVICE_SPECIFIC_$(h)_PATH := $(LOCAL_PATH)/$(call to-lower,$(h))-hal))
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/display \
+    vendor/qcom/opensource/display
